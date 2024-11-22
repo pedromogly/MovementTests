46a06d12e75873b44f77bacd133bba475081f181
Aulas angular <pedromogly@gmail.com>
2024-11-22 18:01:37 -0300

    t2

--- Scenes/player_basic.gd
+++ Scenes/player_basic.gd
@@ -7,12 +7,18 @@ var state = State.IDLE
 var my_velocity:Vector2 = Vector2.ZERO
 var direcao = 1
 
+@export var gravidade:int = 2900
+@export var force_jump = -2000
+@export var raycast_jump:Node2D
+
+@export var sprite: Sprite2D
 @export var animation_player: AnimationPlayer
 
 func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta: float) -> void:
+	apply_gravity(delta)
 	match state:
 		State.IDLE:
 			animation_player.play("idle")
@@ -26,6 +32,8 @@ func _physics_process(delta: float) -> void:
 			if Input.is_action_pressed("Atack"):
 				state = State.ATTACK
 		State.JUMP:
+			if verify_floor():
+				jump()
 			pass
 		State.RUN:
 			move()
@@ -34,6 +42,26 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = my_velocity
 	move_and_slide()
+	
+
+func jump():
+	my_velocity.y = force_jump / 2
+	state = State.IDLE
+
+func apply_gravity(delta):
+	my_velocity.y += gravidade * delta
+
+func verify_floor():
+	for raycast in raycast_jump.get_children():
+		if raycast.is_colliding():
+			return true
+	return false
+
+func flip():
+	if direcao > 0:
+		sprite.flip_h = false
+	elif direcao < 0:
+		sprite.flip_h = true
 
 func move():
 	direcao = int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))
@@ -42,4 +70,5 @@ func move():
 	if my_velocity.x == 0:
 		state = State.IDLE
 	
+	flip()
 	
